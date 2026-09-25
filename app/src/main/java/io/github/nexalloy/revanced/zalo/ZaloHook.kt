77da0eeb4842cc14a4c03b04b3c1b70ccccd8398
XES SEX <117189041+gnadgnaoh@users.noreply.github.com>
2026-09-25 15:27:50 +0700

    Update ZaloHook.kt

--- app/src/main/java/io/github/nexalloy/revanced/zalo/ZaloHook.kt
+++ app/src/main/java/io/github/nexalloy/revanced/zalo/ZaloHook.kt
@@ -1,16 +1,23 @@
 package io.github.nexalloy.revanced.zalo
 
 import io.github.nexalloy.Patch
+import io.github.nexalloy.revanced.zalo.ads.DisableSponsoredPlacements
 import io.github.nexalloy.revanced.zalo.ads.FilterFeedAds
 import io.github.nexalloy.revanced.zalo.ads.HideFeedZInstantAds
 import io.github.nexalloy.revanced.zalo.ads.HideShortVideoAds
 import io.github.nexalloy.revanced.zalo.ads.HideStoryAds
 import io.github.nexalloy.revanced.zalo.ads.SkipFeedAdsBinding
 import io.github.nexalloy.revanced.zalo.adtima.DisableAdtimaAdRequests
+import io.github.nexalloy.revanced.zalo.adtima.DisableAdtimaOfflineAdsAndTracking
 import io.github.nexalloy.revanced.zalo.adtima.DisableAdtimaVideoAdRequests
+import io.github.nexalloy.revanced.zalo.adtima.OptOutAdtimaAdTracking
+import io.github.nexalloy.revanced.zalo.adtima.RemoveAdtimaGoogleNetworks
 import io.github.nexalloy.revanced.zalo.calls.AutoRecordCalls
+import io.github.nexalloy.revanced.zalo.media.KeepExpiredMediaAccessible
+import io.github.nexalloy.revanced.zalo.media.PreferOriginalPhotoQuality
 import io.github.nexalloy.revanced.zalo.privacy.BlockSeenStatus
 import io.github.nexalloy.revanced.zalo.privacy.BlockTypingStatus
+import io.github.nexalloy.revanced.zalo.telemetry.DisableCrashlytics
 import io.github.nexalloy.revanced.zalo.tracking.DisableAdsTracking
 
 val ZaloPatches = arrayOf<Patch>(
@@ -19,10 +26,17 @@ val ZaloPatches = arrayOf<Patch>(
     FilterFeedAds,
     HideStoryAds,
     HideShortVideoAds,
+    DisableSponsoredPlacements,
     DisableAdtimaAdRequests,
     DisableAdtimaVideoAdRequests,
     DisableAdsTracking,
+    DisableAdtimaOfflineAdsAndTracking,
+    OptOutAdtimaAdTracking,
+    RemoveAdtimaGoogleNetworks,
+    DisableCrashlytics,
     AutoRecordCalls,
     BlockSeenStatus,
     BlockTypingStatus,
+    PreferOriginalPhotoQuality,
+    KeepExpiredMediaAccessible,
 )
