bb710f605ac255df8cc91798e1e0a00d7b564e78
XES SEX <117189041+gnadgnaoh@users.noreply.github.com>
2026-09-26 15:40:38 +0700

    Update ZaloHook.kt

--- app/src/main/java/io/github/nexalloy/revanced/zalo/ZaloHook.kt
+++ app/src/main/java/io/github/nexalloy/revanced/zalo/ZaloHook.kt
@@ -4,10 +4,12 @@ import io.github.nexalloy.Patch
 import io.github.nexalloy.revanced.zalo.ads.DisableSponsoredPlacements
 import io.github.nexalloy.revanced.zalo.ads.FilterFeedAds
 import io.github.nexalloy.revanced.zalo.ads.HideFeedZInstantAds
+import io.github.nexalloy.revanced.zalo.ads.HideMessageZinstantAds
 import io.github.nexalloy.revanced.zalo.ads.HideShortVideoAds
 import io.github.nexalloy.revanced.zalo.ads.HideStoryAds
 import io.github.nexalloy.revanced.zalo.ads.SkipFeedAdsBinding
 import io.github.nexalloy.revanced.zalo.adtima.DisableAdtimaAdRequests
+import io.github.nexalloy.revanced.zalo.backup.EnableDrivePhotoBackup
 import io.github.nexalloy.revanced.zalo.adtima.DisableAdtimaOfflineAdsAndTracking
 import io.github.nexalloy.revanced.zalo.adtima.DisableAdtimaVideoAdRequests
 import io.github.nexalloy.revanced.zalo.adtima.OptOutAdtimaAdTracking
@@ -17,12 +19,15 @@ import io.github.nexalloy.revanced.zalo.media.KeepExpiredMediaAccessible
 import io.github.nexalloy.revanced.zalo.media.PreferOriginalPhotoQuality
 import io.github.nexalloy.revanced.zalo.privacy.BlockSeenStatus
 import io.github.nexalloy.revanced.zalo.privacy.BlockTypingStatus
+import io.github.nexalloy.revanced.zalo.telemetry.DisableAdvertisingId
+import io.github.nexalloy.revanced.zalo.telemetry.DisableAnalyticsDatabase
 import io.github.nexalloy.revanced.zalo.telemetry.DisableCrashlytics
+import io.github.nexalloy.revanced.zalo.telemetry.DisableFirebaseAnalytics
 import io.github.nexalloy.revanced.zalo.tracking.DisableAdsTracking
-import io.github.nexalloy.revanced.zalo.backup.EnableDrivePhotoBackup
 
 val ZaloPatches = arrayOf<Patch>(
     HideFeedZInstantAds,
+    HideMessageZinstantAds,
     SkipFeedAdsBinding,
     FilterFeedAds,
     HideStoryAds,
@@ -35,6 +40,9 @@ val ZaloPatches = arrayOf<Patch>(
     OptOutAdtimaAdTracking,
     RemoveAdtimaGoogleNetworks,
     DisableCrashlytics,
+    DisableAnalyticsDatabase,
+    DisableFirebaseAnalytics,
+    DisableAdvertisingId,
     AutoRecordCalls,
     BlockSeenStatus,
     BlockTypingStatus,
